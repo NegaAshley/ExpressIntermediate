17fca2084d2796717f0a489d016f1dd6a117684f
NegaAshley <ashleymxu@gmail.com>
2018-12-15 23:59:25 +0000

    Conditionals and loops with EJS

--- app.js
+++ app.js
@@ -13,6 +13,16 @@ app.get("/fallinlovewith/:thing", function(req, res){
     res.render("love.ejs", {thingVar: thing});
 });
 
+app.get("/posts", function(req, res){
+    var posts = [
+        {title: "Post1", author: "Suzy"},
+        {title: "My adorable pet bunny", author: "Charlie"},
+        {title: "Can you believe this pomsky?", author: "Ashley"}
+    ];
+    
+    res.render("posts.ejs", {posts: posts});
+});
+
 //Undefined route
 app.get("*", function(req, res){
     console.log("Undefined route requested");
